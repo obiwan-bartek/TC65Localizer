560657af52566bcef68179e5a6b2d25bb21145f4
Administrator <Administrator@192.168.58.128>
2014-12-03 02:05:17 +0100

    Updated packages

---
+++ TC65Localizer/src/com/batorek/tc65localizer/IMlet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.batorek.tc65localizer;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Administrator
+ */
+public class IMlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
